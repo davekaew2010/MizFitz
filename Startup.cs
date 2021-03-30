04dfbc467b0dbe6f760b17391a9120ea45617a55
davekaew2010 <74430971+davekaew2010@users.noreply.github.com>
2021-03-30 09:45:41 -0700

    Add files via upload

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MizFitzCapstone.Startup))]
+namespace MizFitzCapstone
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
